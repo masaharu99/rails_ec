96232030b55ce7dcd6d98e413d5e19fd258355d7
masaharu <uchiyama.masaharu999@gmail.com>
2024-03-09 20:27:37 +0900

    seedsを本番環境のテスト用に変更する

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,24 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   Character.create(name: "Luke", movie: movies.first
+
 item1 = Item.new(name: 'Messi', price: 999999999)
-item1.image.attach(io: File.open(Rails.root.join('app/assets/images/', 'messi.jpeg')))
+# item1.image.attach(io: File.open(Rails.root.join('app/assets/images/', 'messi.jpeg')))
 item1.save!
-item2 = Item.new(name: 'Ronald', price: 999999999)
-image2 = File.open(Rails.root.join('app/assets/images/ronald.jpeg').to_s)
-item2.image.attach(io: image2, filename: 'ronald.jpeg')
-item2.save!
-item3 = Item.new(name: 'Neymar', price: 999999999)
-image3 = File.open(Rails.root.join('app/assets/images/neymar.jpeg').to_s)
-item3.image.attach(io: image3, filename: 'neymar.jpeg')
-item3.save!
-item4 = Item.new(name: 'Mbappe', price: 999999999)
-image4 = File.open(Rails.root.join('app/assets/images/mbappe.jpeg').to_s)
-item4.image.attach(io: image4, filename: 'mbappe.jpeg')
-item4.save!
-item5 = Item.new(name: 'De Bruye', price: 999999999)
-image5 = File.open(Rails.root.join('app/assets/images/de_bruyne.jpeg').to_s)
-item5.image.attach(io: image5, filename: 'de_bruyne.jpeg')
-item5.save!
+# item2 = Item.new(name: 'Ronald', price: 999999999)
+# image2 = File.open(Rails.root.join('app/assets/images/ronald.jpeg').to_s)
+# item2.image.attach(io: image2, filename: 'ronald.jpeg')
+# item2.save!
+# item3 = Item.new(name: 'Neymar', price: 999999999)
+# image3 = File.open(Rails.root.join('app/assets/images/neymar.jpeg').to_s)
+# item3.image.attach(io: image3, filename: 'neymar.jpeg')
+# item3.save!
+# item4 = Item.new(name: 'Mbappe', price: 999999999)
+# image4 = File.open(Rails.root.join('app/assets/images/mbappe.jpeg').to_s)
+# item4.image.attach(io: image4, filename: 'mbappe.jpeg')
+# item4.save!
+# item5 = Item.new(name: 'De Bruye', price: 999999999)
+# image5 = File.open(Rails.root.join('app/assets/images/de_bruyne.jpeg').to_s)
+# item5.image.attach(io: image5, filename: 'de_bruyne.jpeg')
+# item5.save!
